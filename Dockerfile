692f40dbd9c6ca48da184cef74f63b1c7d0a1bea
Aleksandr Poliakov <aleksandr.poliakov@clearmatics.com>
2019-04-12 18:22:28 +0100

    Load validators and observers

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,11 @@
-FROM clearmatics/autonity:latest
+FROM python:3.7.3-alpine
 
-RUN apk add --no-cache --virtual python3 .build-deps gcc musl-dev && \
-    python3 -m ensurepip && \
-    rm -r /usr/lib/python*/ensurepip && \
-    pip3 install --upgrade pip setuptools && \
-    if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip ; fi && \
-    if [[ ! -e /usr/bin/python ]]; then ln -sf /usr/bin/python3 /usr/bin/python; fi
+RUN apk add --no-cache --virtual .build-deps gcc musl-dev
 
 WORKDIR /env/genesis-configurator
 ADD ./requirements.txt .
 RUN pip install -r ./requirements.txt
-#RUN apk del .build-deps gcc musl-dev
+RUN apk del .build-deps gcc musl-dev
 COPY ./genesis-configurator.py .
 
 ENTRYPOINT ["/env/genesis-configurator/genesis-configurator.py"]
--- genesis-configurator.py
+++ genesis-configurator.py
@@ -1,19 +1,54 @@
 #!/usr/bin/env python
 
 import json
+from os import listdir
+from os.path import isfile, join, isdir
 
 path_genesis_template = '/autonity/genesis-template.json'
+path_validators = '/autonity/validators'
+path_observers = '/autonity/observers'
 
 
 def get_genesis_template():
-    print('Start')
     with open(path_genesis_template) as f:
         genesis = json.load(f)
     return genesis
 
 
+def get_keys(path):
+    list_vars = [f for f in listdir(path) if isfile(join(path, f))]
+    addresses = {}
+    pub_keys = {}
+    for key_name in list_vars:
+        splitted_key_name = [x.strip() for x in key_name.split('.')]
+        if splitted_key_name[1] == 'address':
+            f = open(path + '/' + key_name, 'r')
+            addresses[splitted_key_name[0]] = f.readline().splitlines()[0]
+            f.close()
+        elif splitted_key_name[1] == 'pub_key':
+            f = open(path + '/' + key_name, 'r')
+            pub_keys[splitted_key_name[0]] = f.readline().splitlines()[0]
+            f.close()
+    return {'addresses': addresses, 'pub_keys': pub_keys}
+
+
+def patch_genesis(genesis, validators, observers):
+    print('======Validators ======')
+
+    # List allowed addresses
+    print(list(validators['addresses'].values()))
+    print('======Observers ======')
+    print(observers)
+    return genesis
+
+
 def main():
-    print(get_genesis_template())
+    genesis = get_genesis_template()
+    validators = get_keys(path_validators)
+    observers = get_keys(path_observers)
+    genesis = patch_genesis(genesis, validators, observers)
+
+    print(genesis)
 
 
 if __name__ == '__main__':
